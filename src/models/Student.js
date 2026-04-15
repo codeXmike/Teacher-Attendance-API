76cc274ee022d4d588d50ac286793b2daf46b228
JoeCode <jctech333@gmail.com>
2026-04-15 07:54:33 +0100

    student model

--- src/models/Student.js
+++ src/models/Student.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema(
   {
-    lecturerId: { type: mongoose.Schema.Types.ObjectId, ref: "Lecturer", required: true, index: true },
     name: { type: String, required: true },
     matricNo: { type: String, required: true, trim: true },
     passwordHash: { type: String, required: true },
@@ -11,6 +10,6 @@ const studentSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-studentSchema.index({ lecturerId: 1, matricNo: 1 }, { unique: true });
+studentSchema.index({ matricNo: 1 }, { unique: true });
 
 export const Student = mongoose.model("Student", studentSchema);
